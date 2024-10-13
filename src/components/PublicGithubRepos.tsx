6f2d5315e5fe8e20cb05ed0d6b7366988caffd9e
Kevin Bervoets <kevin.bervoets@proton.me>
2024-10-14 01:34:38 +0200

    fix button placement in public repo section

--- src/components/PublicGithubRepos.tsx
+++ src/components/PublicGithubRepos.tsx
@@ -32,26 +32,29 @@ export const PublicGithubRepos = () => {
         {publicGithubRepos && (
             <div className="grid grid-cols-6 gap-2 mb-1">
                 {publicGithubRepos.map((repo) => (
-                    <div key={repo.id} className="border rounded-2xl px-6 py-4">
+                    <div key={repo.id} className="border rounded-2xl px-6 py-4 flex flex-col justify-between">
                         <div className="mb-6">
                             <h4 className="text-md font-bold">{repo.repoName}</h4>
                             <span className="text-xs text-violet-400">{repo.owner.toUpperCase()}</span>
                         </div>
 
-                        <button
-                            className="border border-blue-700 rounded-full w-full mb-4 px-4 py-2 text-blue-700 hover:bg-blue-700 hover:text-white"
-                            onClick={() => getCommitsForRepo(repo.owner, repo.repoName)}>
-                            Show commits
-                        </button>
-                        <a
-                            href={repo.htmlUrl}
-                            target="_blank"
-                            rel="noopener noreferrer">
+                        <div>
                             <button
-                                className="border border-blue-700 rounded-full w-full px-4 py-2 text-blue-700 hover:bg-blue-700 hover:text-white">To
-                                github
+                                className="border border-blue-700 rounded-full w-full mb-4 px-4 py-2 text-blue-700 hover:bg-blue-700 hover:text-white"
+                                onClick={() => getCommitsForRepo(repo.owner, repo.repoName)}>
+                                Show commits
                             </button>
-                        </a>
+                            <a
+                                href={repo.htmlUrl}
+                                target="_blank"
+                                rel="noopener noreferrer">
+                                <button
+                                    className="border border-blue-700 rounded-full w-full px-4 py-2 text-blue-700 hover:bg-blue-700 hover:text-white">To
+                                    github
+                                </button>
+                            </a>
+                        </div>
+
                     </div>
 
                 ))}
