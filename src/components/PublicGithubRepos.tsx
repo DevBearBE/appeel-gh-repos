5fa9cb30fee27f48696e2c293e02d8a1d0f68982
Kevin Bervoets <kevin.bervoets@proton.me>
2024-10-14 22:25:03 +0200

    implement show commits improvement

--- src/components/PublicGithubRepos.tsx
+++ src/components/PublicGithubRepos.tsx
@@ -1,13 +1,15 @@
 import { useAtom } from "jotai";
 import { useErrorBoundary } from "react-error-boundary";
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { getCommitsForPublicGithubRepo, getPublicGithubRepos } from "../utils/helpers";
-import { commitsAtom, publicGithubReposAtom } from "../state";
+import { commitsAtom, PublicGithubRepo, publicGithubReposAtom } from "../state";
 
 export const PublicGithubRepos = () => {
     const {showBoundary} = useErrorBoundary();
     const [publicGithubRepos, setPublicGithubRepos] = useAtom(publicGithubReposAtom);
     const [commitsForRepo, setCommitsForRepo] = useAtom(commitsAtom);
+    const [selectedRepo, setSelectedRepo] = useState<PublicGithubRepo>({} as PublicGithubRepo);
+    const [showCommits, setShowCommits] = useState<boolean>(false);
 
     const fetchPublicGithubRepos = useCallback(async () => {
         try {
@@ -22,14 +24,19 @@ export const PublicGithubRepos = () => {
         fetchPublicGithubRepos();
     }, []);
 
-    const getCommitsForRepo = async (owner: string, repo: string) => {
-        const commits = await getCommitsForPublicGithubRepo(owner, repo);
+    const getCommitsForRepo = async (repo: PublicGithubRepo) => {
+        const {owner, repoName} = repo;
+        setSelectedRepo(repo);
+
+        const commits = await getCommitsForPublicGithubRepo(owner, repoName);
         setCommitsForRepo(commits);
+
+        setShowCommits(true);
     }
 
     return (<div>
         <h3 className="text-lg text-blue-700 font-bold my-4">Public repositories</h3>
-        {publicGithubRepos && (
+        {!showCommits && publicGithubRepos && (
             <div className="grid grid-cols-6 gap-2 mb-6">
                 {publicGithubRepos.map((repo) => (
                     <div key={repo.id} className="border rounded-2xl px-6 py-4 flex flex-col justify-between">
@@ -41,7 +48,7 @@ export const PublicGithubRepos = () => {
                         <div>
                             <button
                                 className="border border-sky-600 rounded-full w-full mb-4 px-4 py-1 text-sky-600 hover:bg-sky-600 hover:text-white"
-                                onClick={() => getCommitsForRepo(repo.owner, repo.repoName)}>
+                                onClick={() => getCommitsForRepo(repo)}>
                                 Show commits
                             </button>
                             <a
@@ -59,11 +66,33 @@ export const PublicGithubRepos = () => {
             </div>
         )}
 
-        {commitsForRepo && commitsForRepo.map((commit) => (
-            <div key={commit.id} className="mb-2">
-                <p className="-mb-1">{commit.message}</p>
-                <span className="text-xs text-violet-400">{commit.authorName.toUpperCase()}</span>
+        {showCommits && commitsForRepo && (
+            <div>
+                <div className="mb-6 flex justify-between">
+                    <div className="border rounded-2xl px-6 py-4">
+                        <h4 className="text-md font-bold">{selectedRepo.repoName}</h4>
+                        <span className="text-xs text-violet-400">{selectedRepo.owner.toUpperCase()}</span>
+                    </div>
+                    <div>
+                        <button
+                            className="border border-sky-600 rounded-full mb-4 px-4 py-1 text-sky-600 hover:bg-sky-600 hover:text-white"
+                            onClick={() => setShowCommits(false)}>
+                            Back
+                        </button>
+                    </div>
+                </div>
+                <h4 className="text-md font-bold mb-2">Commits</h4>
+                {commitsForRepo.map((commit) => (
+                    <div key={commit.id} className="mb-2">
+                        <div>
+                            <p className="-mb-1">{commit.message}</p>
+                            <span className="text-xs text-violet-400">{commit.authorName.toUpperCase()}</span>
+                        </div>
+
+                    </div>
+                ))}
             </div>
-        ))}
+        )
+        }
     </div>)
 }
